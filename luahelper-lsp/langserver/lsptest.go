8bd8856f457bfd2eb4cfcc8a8eb5bcca713a365a
ggshily <gg.shily@gmail.com>
2021-12-10 16:22:44 +0800

    支持扩展全局table

--- luahelper-lsp/langserver/lsptest.go
+++ luahelper-lsp/langserver/lsptest.go
@@ -31,3 +31,30 @@ func createLspTest(strRootPath string, strRootUri string) *LspServer{
 	lspServer.Initialize(context, initializeParams)
 	return lspServer
 }
+
+func createLspTestWithPlugin(strRootPath string, strRootUri string, pluginPath string) *LspServer{
+	common.GlobalConfigDefautInit()
+	common.GConfig.IntialGlobalVar()
+	
+	lspServer := CreateLspServer()
+	lspServer.server = jrpc2.NewServer(handler.Map{}, &jrpc2.ServerOptions{
+		AllowPush:   false,
+		Concurrency: 1,
+	})
+
+	context := context.Background()
+	initializeParams := InitializeParams{
+		InitializeParams : lsp.InitializeParams {
+			InnerInitializeParams : lsp.InnerInitializeParams{
+				RootPath : strRootPath,
+				RootURI: lsp.DocumentURI(strRootUri),
+			},
+		},
+		InitializationOptions: &InitializationOptions{
+			AllEnable:  true,
+			PluginPath: pluginPath,
+		},
+	}
+	lspServer.Initialize(context, initializeParams)
+	return lspServer
+}
