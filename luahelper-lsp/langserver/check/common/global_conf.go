0342b7c2731bf986c5183c5737d5eceecc94c8d2
yinfei8 <yinfei1988@gmail.com>
2022-03-01 13:45:19 +0800

    1 ) 成员变量告警增加是否检查标记

--- luahelper-lsp/langserver/check/common/global_conf.go
+++ luahelper-lsp/langserver/check/common/global_conf.go
@@ -661,6 +661,7 @@ func (g *GlobalConfig) ReadConfig(strDir, configFileName string, checkFlagList [
 	for _, errorType := range jsonConfig.IgnoreErrorTypes {
 		g.IgnoreErrorTypeMap[(CheckErrorType)(errorType)] = true
 	}
+	g.IgnoreErrorTypeMap[CheckErrorClassField] = true
 
 	// 忽略对某些文件或文件夹进行check分析， 包含go语言的正则
 	g.IgnoreHandleFolderVec = make([]string, 0, 2)
