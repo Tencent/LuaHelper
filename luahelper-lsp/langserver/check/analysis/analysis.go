6b1e33d5ef68ba6c0e669857a8b06da97da948b4
RichardYin <richbirdandy@163.com>
2022-03-02 16:01:10 +0800

    支持const

--- luahelper-lsp/langserver/check/analysis/analysis.go
+++ luahelper-lsp/langserver/check/analysis/analysis.go
@@ -426,6 +426,9 @@ func (a *Analysis) CheckTableDecl(strTableName string, strFieldNamelist []string
 	if common.GConfig.IsGlobalIgnoreErrType(common.CheckErrorClassField) {
 		return
 	}
+	// if strTableName == "tableA" {
+	// 	strTableName = "tableA"
+	// }
 
 	if strTableName == "" || len(strFieldNamelist) == 0 || nodeLoc == nil || node == nil {
 		return
@@ -453,9 +456,63 @@ func (a *Analysis) CheckTableDecl(strTableName string, strFieldNamelist []string
 			}
 		}
 	}
+	return
 }
 
-// 根据注解判断table成员合法性 包括 t.a t可以是符号 或者函数参数  todo todo
+func (a *Analysis) FindVarDefineForCheck(varName string, loc lexer.Location) (find bool, varInfo *common.VarInfo) {
+
+	find = false
+	//先尝试找local变量
+	varInfo, find = a.curScope.FindLocVar(varName, loc)
+	if find {
+		return find, varInfo
+	}
+
+	//没找到就找全局变量
+	fi := a.curFunc
+	firstFile := a.getFirstFileResult(a.curResult.Name)
+
+	gFlag := false
+	strName := varName
+	strProPre := ""
+
+	fileResult := a.curResult
+	if a.isSecondTerm() {
+		secondFileResult := fileResult
+		if fi.FuncLv == 0 {
+			// 最顶层的函数，只在前面的定义中查找
+			find, varInfo = secondFileResult.FindGlobalVarInfo(strName, gFlag, strProPre)
+			if !find {
+				find, varInfo = a.SingleProjectResult.FindGlobalGInfo(strName, results.CheckTermSecond, strProPre)
+			}
+		} else {
+			// 非底层的函数，需要查找全局的变量
+			find, varInfo = firstFile.FindGlobalVarInfo(strName, gFlag, strProPre)
+			if !find {
+				find, varInfo = a.SingleProjectResult.FindGlobalGInfo(strName, results.CheckTermFirst, strProPre)
+			}
+		}
+	} else if a.isThirdTerm() {
+		thirdFileResult := fileResult
+		if fi.FuncLv == 0 {
+			// 最顶层的函数，只在前面的定义中查找
+			find, varInfo = thirdFileResult.FindGlobalVarInfo(strName, gFlag, strProPre)
+		} else {
+			// 非底层的函数，需要查找全局的变量
+			find, varInfo = firstFile.FindGlobalVarInfo(strName, gFlag, strProPre)
+		}
+
+		// 查找所有的
+		if !find {
+			find, varInfo = a.AnalysisThird.ThirdStruct.FindThirdGlobalGInfo(gFlag, strName, strProPre)
+		}
+	}
+
+	return find, varInfo
+
+}
+
+// 根据注解判断table成员合法性 包括 t.a t可以是符号 或者函数参数
 func (a *Analysis) checkTableAccess(node *ast.TableAccessExp) {
 	if !a.isNeedCheck() || a.realTimeFlag {
 		return
@@ -471,80 +528,18 @@ func (a *Analysis) checkTableAccess(node *ast.TableAccessExp) {
 		return
 	}
 
+	// if strTableName == "tableA" {
+	// 	strTableName = "tableA"
+	// }
+
 	strKey := common.GetExpName(node.KeyExp)
 	// 如果不是简单字符，退出
 	if !common.JudgeSimpleStr(strKey) || strKey == "" {
 		return
 	}
 
-	find := false
-	//先尝试找local变量
-	varInfo, ok := a.curScope.FindLocVar(strTableName, node.Loc)
-	if ok {
-		//decLine = locVarInfo.Loc.StartLine - 1
-		find = true
-	} else {
-		fi := a.curFunc
-		firstFile := a.getFirstFileResult(a.curResult.Name)
-
-		gFlag := false
-		strName := strTableName
-		strProPre := ""
-
-		// 4) 第三步查找全局中是否有该变量
-		fileResult := a.curResult
-		if a.isSecondTerm() {
-			secondFileResult := fileResult
-			if fi.FuncLv == 0 {
-				// 最顶层的函数，只在前面的定义中查找
-				ok, varInfo = secondFileResult.FindGlobalVarInfo(strName, gFlag, strProPre)
-				if ok {
-					find = true
-				} else {
-					ok, varInfo = a.SingleProjectResult.FindGlobalGInfo(strName, results.CheckTermSecond, strProPre)
-					if ok {
-						find = true
-					}
-				}
-			} else {
-				// 非底层的函数，需要查找全局的变量
-				ok, varInfo = firstFile.FindGlobalVarInfo(strName, gFlag, strProPre)
-				if ok {
-					find = true
-				} else {
-					ok, varInfo = a.SingleProjectResult.FindGlobalGInfo(strName, results.CheckTermFirst, strProPre)
-					if ok {
-						find = true
-					}
-				}
-			}
-		} else if a.isThirdTerm() {
-			thirdFileResult := fileResult
-			if fi.FuncLv == 0 {
-				// 最顶层的函数，只在前面的定义中查找
-				ok, varInfo = thirdFileResult.FindGlobalVarInfo(strName, gFlag, strProPre)
-				if ok {
-					find = true
-				}
-			} else {
-				// 非底层的函数，需要查找全局的变量
-				ok, varInfo = firstFile.FindGlobalVarInfo(strName, gFlag, strProPre)
-				if ok {
-					find = true
-				}
-			}
-
-			// 查找所有的
-			if !find {
-				ok, varInfo = a.AnalysisThird.ThirdStruct.FindThirdGlobalGInfo(gFlag, strName, strProPre)
-				if ok {
-					find = true
-				}
-			}
-		}
-	}
-
-	if !find {
+	ok, varInfo := a.FindVarDefineForCheck(strTableName, node.Loc)
+	if !ok {
 		return
 	}
 
@@ -560,4 +555,37 @@ func (a *Analysis) checkTableAccess(node *ast.TableAccessExp) {
 		a.curResult.InsertError(common.CheckErrorSelfAssign, errStr, node.Loc)
 		//a.curResult.InsertError(common.CheckErrorClassField, errStr, node.Loc)
 	}
+
+	return
+
+}
+
+// 是否给常量赋值
+func (a *Analysis) checkConstAssgin(node ast.Exp) {
+	if !a.isNeedCheck() || a.realTimeFlag {
+		return
+	}
+
+	// if common.GConfig.IsGlobalIgnoreErrType(common.CheckErrorClassField) {
+	// 	return
+	// }
+
+	// name := ""
+	// loc := lexer.Location{}
+	// switch exp := node.(type) {
+	// case *ast.NameExp:
+	// 	name = exp.Name
+	// 	loc = exp.Loc
+	// 	//case *ast.ParensExp:
+	// 	//loc = exp
+	// 	// case *ast.TableConstructorExp:
+	// 	// 	name = exp.ValExps
+	// }
+
+	// ok, varInfo := a.FindVarDefineForCheck(name, loc)
+	// if !ok {
+	// 	return
+	// }
+
+	// a.Projects.GetAnnotateClass(a.curResult.Name, name, varInfo, loc.StartLine-1, a.curScope)
 }
