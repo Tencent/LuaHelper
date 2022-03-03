cff2c9dfa881e051efcded5376be12ed3d18785d
yinfei8 <yinfei1988@gmail.com>
2022-03-03 17:18:31 +0800

    1）hover增加新的测试用例 2）complete增加新的测试用例

--- luahelper-lsp/langserver/textdocument_hover_test.go
+++ luahelper-lsp/langserver/textdocument_hover_test.go
@@ -1423,3 +1423,68 @@ func TestHoverExpandNo(t *testing.T) {
 		}
 	}
 }
+
+// hover 扩展显示函数的返回值
+func TestHoverFuncReturn(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	paths, _ := filepath.Split(filename)
+
+	strRootPath := paths + "../testdata/hover"
+	strRootPath, _ = filepath.Abs(strRootPath)
+
+	strRootURI := "file://" + strRootPath
+	lspServer := createLspTest(strRootPath, strRootURI)
+	context := context.Background()
+
+	fileName := strRootPath + "/" + "hover_func_return.lua"
+	data, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		t.Fatalf("read file:%s err=%s", fileName, err.Error())
+	}
+	openParams := lsp.DidOpenTextDocumentParams{
+		TextDocument: lsp.TextDocumentItem{
+			URI:  lsp.DocumentURI(fileName),
+			Text: string(data),
+		},
+	}
+	err1 := lspServer.TextDocumentDidOpen(context, openParams)
+	if err1 != nil {
+		t.Fatalf("didopen file:%s err=%s", fileName, err1.Error())
+	}
+
+	var resultList [][]string = [][]string{}
+	var positionList []lsp.Position = []lsp.Position{}
+	positionList = append(positionList, lsp.Position{
+		Line:      0,
+		Character: 13,
+	})
+	resultList = append(resultList, []string{"->1. string", "->2. string", "->3. string"})
+
+	positionList = append(positionList, lsp.Position{
+		Line:      5,
+		Character: 18,
+	})
+	resultList = append(resultList, []string{"->1. string", "->2. string", "->3. function", "one: string", "two: table", "c: any"})
+
+	for index, onePoisiton := range positionList {
+		hoverParams := lsp.TextDocumentPositionParams{
+			TextDocument: lsp.TextDocumentIdentifier{
+				URI: lsp.DocumentURI(fileName),
+			},
+			Position: onePoisiton,
+		}
+		hoverReturn1, err1 := lspServer.TextDocumentHover(context, hoverParams)
+		if err1 != nil {
+			t.Fatalf("TextDocumentHover file:%s err=%s", fileName, err1.Error())
+		}
+
+		hoverMarkUpReturn1, _ := hoverReturn1.(MarkupHover)
+
+		for _, oneStr := range resultList[index] {
+			if !strings.Contains(hoverMarkUpReturn1.Contents.Value, oneStr) {
+				t.Fatalf("hover error, not find str=%s, index=%d", oneStr, index)
+			}
+		}
+	}
+}
