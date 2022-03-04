55ac0e7dd9c99afb2f4093c59de916e0600739bb
yinfei8 <yinfei1988@gmail.com>
2022-03-04 14:21:59 +0800

    1）代码补全代码优化 2）基于历史代码补全增加测试用例

---
+++ luahelper-lsp/testdata/complete/test_intelligent1.lua
@@ -0,0 +1,17 @@
+abcdd = {}
+print(abcdd.a)
+abcdd.addd().ddd.dddp()
+abcdd.eff()
+abcdd.addd().ddd.dddp()
+abcdd.eff()
+abcdd:dd().dd1()
+abcdd:dd().dd1()
+
+print(abcdd.addd().ddd.a.d)
+print(abcdd.addd().ddd.dd.ddd)
+
+abcdd:dd()
+abcdd.ccc(one, one).a.b.c()
+
+      
+
